3c2b1ea53eeae6c79b2a36d238d41181a6654b92
wkblev01 <129453825+wkblev01@users.noreply.github.com>
2023-04-05 23:24:54 -0400

    Add files via upload

---
+++ CSharp-Program-Sales-Cost-Form/Full Program/Program3/SalesCostCalculator.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Program3
+{
+    partial class SalesCostCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.stateLbl = new System.Windows.Forms.Label();
+            this.productLbl = new System.Windows.Forms.Label();
+            this.quantityLbl = new System.Windows.Forms.Label();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.stateComboBox = new System.Windows.Forms.ComboBox();
+            this.productTxtBox = new System.Windows.Forms.TextBox();
+            this.quantityTxtBox = new System.Windows.Forms.TextBox();
+            this.initCostLbl = new System.Windows.Forms.Label();
+            this.discCostLbl = new System.Windows.Forms.Label();
+            this.taxLbl = new System.Windows.Forms.Label();
+            this.totalCostLbl = new System.Windows.Forms.Label();
+            this.initCostOutLbl = new System.Windows.Forms.Label();
+            this.discCostOutLbl = new System.Windows.Forms.Label();
+            this.taxOutLbl = new System.Windows.Forms.Label();
+            this.totalCostOutLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // stateLbl
+            // 
+            this.stateLbl.AutoSize = true;
+            this.stateLbl.Location = new System.Drawing.Point(70, 16);
+            this.stateLbl.Name = "stateLbl";
+            this.stateLbl.Size = new System.Drawing.Size(35, 13);
+            this.stateLbl.TabIndex = 0;
+            this.stateLbl.Text = "State:";
+            // 
+            // productLbl
+            // 
+            this.productLbl.AutoSize = true;
+            this.productLbl.Location = new System.Drawing.Point(58, 43);
+            this.productLbl.Name = "productLbl";
+            this.productLbl.Size = new System.Drawing.Size(47, 13);
+            this.productLbl.TabIndex = 1;
+            this.productLbl.Text = "Product:";
+            // 
+            // quantityLbl
+            // 
+            this.quantityLbl.AutoSize = true;
+            this.quantityLbl.Location = new System.Drawing.Point(56, 68);
+            this.quantityLbl.Name = "quantityLbl";
+            this.quantityLbl.Size = new System.Drawing.Size(49, 13);
+            this.quantityLbl.TabIndex = 2;
+            this.quantityLbl.Text = "Quantity:";
+            // 
+            // calcButton
+            // 
+            this.calcButton.Location = new System.Drawing.Point(118, 103);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(75, 23);
+            this.calcButton.TabIndex = 3;
+            this.calcButton.Text = "Calculate";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // stateComboBox
+            // 
+            this.stateComboBox.FormattingEnabled = true;
+            this.stateComboBox.Items.AddRange(new object[] {
+            "KY",
+            "OH",
+            "IN",
+            "IL"});
+            this.stateComboBox.Location = new System.Drawing.Point(108, 13);
+            this.stateComboBox.Name = "stateComboBox";
+            this.stateComboBox.Size = new System.Drawing.Size(121, 21);
+            this.stateComboBox.TabIndex = 4;
+            // 
+            // productTxtBox
+            // 
+            this.productTxtBox.Location = new System.Drawing.Point(108, 40);
+            this.productTxtBox.Name = "productTxtBox";
+            this.productTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.productTxtBox.TabIndex = 5;
+            // 
+            // quantityTxtBox
+            // 
+            this.quantityTxtBox.Location = new System.Drawing.Point(108, 65);
+            this.quantityTxtBox.Name = "quantityTxtBox";
+            this.quantityTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.quantityTxtBox.TabIndex = 6;
+            // 
+            // initCostLbl
+            // 
+            this.initCostLbl.AutoSize = true;
+            this.initCostLbl.Location = new System.Drawing.Point(44, 147);
+            this.initCostLbl.Name = "initCostLbl";
+            this.initCostLbl.Size = new System.Drawing.Size(58, 13);
+            this.initCostLbl.TabIndex = 7;
+            this.initCostLbl.Text = "Initial Cost:";
+            // 
+            // discCostLbl
+            // 
+            this.discCostLbl.AutoSize = true;
+            this.discCostLbl.Location = new System.Drawing.Point(14, 178);
+            this.discCostLbl.Name = "discCostLbl";
+            this.discCostLbl.Size = new System.Drawing.Size(88, 13);
+            this.discCostLbl.TabIndex = 8;
+            this.discCostLbl.Text = "Discounted Cost:";
+            // 
+            // taxLbl
+            // 
+            this.taxLbl.AutoSize = true;
+            this.taxLbl.Location = new System.Drawing.Point(74, 212);
+            this.taxLbl.Name = "taxLbl";
+            this.taxLbl.Size = new System.Drawing.Size(28, 13);
+            this.taxLbl.TabIndex = 9;
+            this.taxLbl.Text = "Tax:";
+            // 
+            // totalCostLbl
+            // 
+            this.totalCostLbl.AutoSize = true;
+            this.totalCostLbl.Location = new System.Drawing.Point(44, 246);
+            this.totalCostLbl.Name = "totalCostLbl";
+            this.totalCostLbl.Size = new System.Drawing.Size(58, 13);
+            this.totalCostLbl.TabIndex = 10;
+            this.totalCostLbl.Text = "Total Cost:";
+            // 
+            // initCostOutLbl
+            // 
+            this.initCostOutLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.initCostOutLbl.Location = new System.Drawing.Point(108, 146);
+            this.initCostOutLbl.Name = "initCostOutLbl";
+            this.initCostOutLbl.Size = new System.Drawing.Size(100, 18);
+            this.initCostOutLbl.TabIndex = 11;
+            // 
+            // discCostOutLbl
+            // 
+            this.discCostOutLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.discCostOutLbl.Location = new System.Drawing.Point(108, 177);
+            this.discCostOutLbl.Name = "discCostOutLbl";
+            this.discCostOutLbl.Size = new System.Drawing.Size(100, 18);
+            this.discCostOutLbl.TabIndex = 12;
+            // 
+            // taxOutLbl
+            // 
+            this.taxOutLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.taxOutLbl.Location = new System.Drawing.Point(108, 211);
+            this.taxOutLbl.Name = "taxOutLbl";
+            this.taxOutLbl.Size = new System.Drawing.Size(100, 18);
+            this.taxOutLbl.TabIndex = 13;
+            // 
+            // totalCostOutLbl
+            // 
+            this.totalCostOutLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.totalCostOutLbl.Location = new System.Drawing.Point(108, 245);
+            this.totalCostOutLbl.Name = "totalCostOutLbl";
+            this.totalCostOutLbl.Size = new System.Drawing.Size(100, 18);
+            this.totalCostOutLbl.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.calcButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 306);
+            this.Controls.Add(this.totalCostOutLbl);
+            this.Controls.Add(this.taxOutLbl);
+            this.Controls.Add(this.discCostOutLbl);
+            this.Controls.Add(this.initCostOutLbl);
+            this.Controls.Add(this.totalCostLbl);
+            this.Controls.Add(this.taxLbl);
+            this.Controls.Add(this.discCostLbl);
+            this.Controls.Add(this.initCostLbl);
+            this.Controls.Add(this.quantityTxtBox);
+            this.Controls.Add(this.productTxtBox);
+            this.Controls.Add(this.stateComboBox);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.quantityLbl);
+            this.Controls.Add(this.productLbl);
+            this.Controls.Add(this.stateLbl);
+            this.Name = "Form1";
+            this.Text = "Sales Cost Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label stateLbl;
+        private System.Windows.Forms.Label productLbl;
+        private System.Windows.Forms.Label quantityLbl;
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.ComboBox stateComboBox;
+        private System.Windows.Forms.TextBox productTxtBox;
+        private System.Windows.Forms.TextBox quantityTxtBox;
+        private System.Windows.Forms.Label initCostLbl;
+        private System.Windows.Forms.Label discCostLbl;
+        private System.Windows.Forms.Label taxLbl;
+        private System.Windows.Forms.Label totalCostLbl;
+        private System.Windows.Forms.Label initCostOutLbl;
+        private System.Windows.Forms.Label discCostOutLbl;
+        private System.Windows.Forms.Label taxOutLbl;
+        private System.Windows.Forms.Label totalCostOutLbl;
+    }
+}
+
---
+++ CSharp-Program-Sales-Cost-Form/Isolated Written Code Example/SalesCostCalculator.cs
@@ -0,0 +1,125 @@
+﻿// Program 3
+// CIS 199-01
+// Due: 11/05/2020
+// By: R1466
+
+// This program creates a form with a combo box to select a state, and accepts text input for a product code and quantity.
+// Form uses parallel arrays and for loops to match inputs with product price, tax, and discount. These totals are then calculated and output to the form.
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program3
+{
+    public partial class SalesCostCalculator : Form
+    {
+        public string[] states = { "KY", "OH", "IN", "IL" };                        // Array that holds states listed in combo box. Used in for loop to determine tax rate.
+        public double[] taxRates = { 0.06, 0.0717, 0.07, 0.0874 };                  // Parallel array that holds state tax rates. Once correct state is found, corresponding tax rate is pulled from here.
+        public int[] products = { 1001, 1002, 1003, 1004, 1005, 1006, 1007 };       // Array that holds product codes. Used in for loop to determine product price per.
+        public double[] prices = { 7.87, 9.51, 10.73, 9.99, 11.99, 5.00, 4.58 };    // Parallel array that holds product prices. Once correct product is found, corresponding price is pulled from here.
+        public int[] qtyLowLimits = { 0, 5, 10, 20 };                               // Array that holds floor value for each discount tier. Used in while loop to determine discount rate.
+        public double[] discountRates = { 0.0, 0.05, 0.10, 0.15 };                  // Parallel array that holds discount rate. Once correct discount tier is found, corresponding rate is pulled from here.
+
+        public const int MIN_QTY = 0;               // Used in TryParse for Quantity to insure a positive value is entered.
+        public const int PRODUCT_MIN = 1001;        // Used in TryParse for Product to insure product code is >= this number.
+        public const int PRODUCT_MAX = 1007;        // Used in TryParse for Product to insure product code is <= this number.
+
+        public int product,             // Variable that holds product code entered. Used in for loop determining product price.
+                quantity,               // Variable that holds quantity of product entered. Used in while loop to find discount rate, and in initial cost calculation. 
+                index;                  // Variable used to set threshold in while loop to determine discount rate.
+        public double costPer,          // Holds applicable product price after found by for loop.
+            taxPercent,                 // Holds applicable tax rate after found by for loop.
+            discountPercent,            // Holds applicable discount rate after found by while loop.
+            initialTotal,               // Holds calculation of initial total and is output to form.
+            discountedTotal,            // Holds calculation of discounted total and is output to form.
+            taxTotal,                   // Holds calculation of tax added and is output to form.
+            grandTotal;                 // Holds calculation of total cost and is output to form.
+
+        public SalesCostCalculator()
+        {
+            InitializeComponent();
+        }
+
+        // Event that triggers upon activation of calcButton.
+        // Input is validated with TryParse in nested if statements. Then, two for loops and one while loop compare inputs to arrays, then provide data via parallel arrays.
+        // Needed totals are then calculated and output to form.
+        private void calcButton_Click(object sender, EventArgs e)
+        {
+            bool validInput = false,        // Boolean variable to hold if TryParse was successful.
+                productFound = false,       // Used to stop for loop once correct product is found in array.
+                stateFound = false,         // Used to stop for loop once correct state is found in array.
+                quantityFound = false;      // Used to stop while loop once correct discount tier is found in array.
+
+
+
+
+            // Tryparse for valid inputs
+            if (stateComboBox.SelectedIndex >=0)        // Checks if a state is chosen from the combo box.
+            {
+                if (int.TryParse(productTxtBox.Text, out product) && product <= PRODUCT_MAX && product >= PRODUCT_MIN)      // Checks if product is an int as well as within valid range of codes.
+                {
+                    if (int.TryParse(quantityTxtBox.Text, out quantity) && quantity > MIN_QTY)          // Checks if quantity is a positive int.
+                        validInput = true;                                                              // True if all TryParses succeed. Used to trigger rest of event code.
+                    else
+                        MessageBox.Show("Please enter a positive quantity of products needed.");    // Message if quantity is not valid.
+                }
+                else
+                    MessageBox.Show("Please enter a valid product code.");                          // Message if product code is not valid.
+            }
+            else
+                MessageBox.Show("Please select a state from the drop-down list.");                  // Message if state is not selected.
+
+
+            // Calculation and Output
+            if (validInput)                 // Only triggers if TryParses succeed.
+            {
+                // Find Product Cost
+                for (int i = 0; i < products.Length && !productFound; ++i)      // Loops through products array until match is found.
+                    if (products[i] == product)
+                    {
+                        productFound = true;                                    // Stops loop once match is found.
+                        costPer = prices[i];                                    // Cost is assigned from parallel array.
+                    }
+
+                // Find Tax Rate
+                for (int i = 0; i < states.Length && !stateFound; ++i)          // Loops through states array until match is found.
+                    if (states[i] == stateComboBox.Text)
+                    {
+                        stateFound = true;                                      // Stops loop once match is found.
+                        taxPercent = taxRates[i];                               // Tax Rate is assigned from parallel array.
+                    }
+
+                // Find Discount
+                index = qtyLowLimits.Length - 1;
+                while (index >= 0 && !quantityFound)                            // Loops through discount floor array until range match is found.
+                {
+                    if (quantity >= qtyLowLimits[index])
+                        quantityFound = true;                                   // Stops loop once match is found.
+                    else
+                        --index;
+                }
+                if (quantityFound)
+                    discountPercent = discountRates[index];                     // Discount Rate is assigned from parallel array.
+
+                // Calculate output variables
+                initialTotal = quantity * costPer;                                          
+                discountedTotal = initialTotal - (initialTotal * discountPercent);
+                taxTotal = discountedTotal * taxPercent;
+                grandTotal = discountedTotal + taxTotal;
+
+                // Output to Form Labels
+                initCostOutLbl.Text = $"{initialTotal.ToString("C")}";
+                discCostOutLbl.Text = $"{discountedTotal.ToString("C")}";
+                taxOutLbl.Text = $"{taxTotal.ToString("C")}";
+                totalCostOutLbl.Text = $"{grandTotal.ToString("C")}";
+            }
+
+        }
+    }
+}
